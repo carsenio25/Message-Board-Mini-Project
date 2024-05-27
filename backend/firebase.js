cf23e4cf326ea3d5803872de0d316a599ccac7d6
carsenio25 <112511512+carsenio25@users.noreply.github.com>
2024-05-27 15:11:51 -0400

    update firebase setup

--- backend/firebase.js
+++ backend/firebase.js
@@ -9,14 +9,7 @@ const { getFirestore } = require("firebase/firestore");
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    // apiKey: "AIzaSyAS8W7N_b4UE74eBqFcw2t3yygdJ0918FM",
-    // authDomain: "message-board-736ef.firebaseapp.com",
-    // projectId: "message-board-736ef",
-    // storageBucket: "message-board-736ef.appspot.com",
-    // messagingSenderId: "499927581139",
-    // appId: "1:499927581139:web:17e84264ca76155e50af26",
-    // measurementId: "G-C4J5910F3G"
-        apiKey: process.env.API_KEY,
+    apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
     projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
@@ -30,30 +23,3 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 module.exports = db;
-
-
-// require('dotenv').config();
-
-// // Import the functions you need from the SDKs you need
-// const { initializeApp } = require("firebase/app");
-// const { getFirestore } = require("firebase/firestore");
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: process.env.API_KEY,
-//     authDomain: process.env.AUTH_DOMAIN,
-//     projectId: process.env.PROJECT_ID,
-//     storageBucket: process.env.STORAGE_BUCKET,
-//     messagingSenderId: process.env.MESSAGING_SENDER_ID,
-//     appId: process.env.APP_ID,
-//     measurementId: process.env.MEASUREMENT_ID
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-
-// module.exports = db;
